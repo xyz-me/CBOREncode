440acd8baeb54d5547075e99b8074248d8d2eba4
shutaro.tamada <tsyutaro0121@gmail.com>
2025-01-18 23:16:33 +0900

    CBORByteStringクラスにgetData()メソッドと__toString()メソッドを追加

--- src/CBOR/Types/CBORByteString.php
+++ src/CBOR/Types/CBORByteString.php
@@ -9,17 +9,23 @@ class CBORByteString {
         $this->byte_string = $byte_string;
     }
 
-    /**
-     * @return null
-     */
+    // バイト列を取得するための「getData()」メソッド
+    public function getData()
+    {
+        return $this->byte_string;
+    }
+
+    // 文字列キャスト時にバイト列を返す
+    public function __toString()
+    {
+        return (string)$this->byte_string;
+    }
+
     public function get_byte_string()
     {
         return $this->byte_string;
     }
 
-    /**
-     * @param null $byte_string
-     */
     public function set_byte_string($byte_string)
     {
         $this->byte_string = $byte_string;
